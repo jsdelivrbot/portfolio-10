bff75c74723e97b55acb1fb8f06fbdf03a5d91b0
Kevin <katokh89@gmail.com>
2016-06-10 17:18:02 -0700

    begin setting up travelbug project page.

--- app/components/Travelbug.jsx
+++ app/components/Travelbug.jsx
@@ -6,7 +6,27 @@ var Travelbug = React.createClass({
     return (
       <div>
         <Nav />
-        <h1>Sup</h1>
+        <h4>travelbug</h4>
+        <div class="section info-section">
+          <div class="info">
+            <div class="info-title info-type">type</div>
+            <div class="info-content">
+              <div class="button grey">web app</div>
+            </div>
+            <div class="info-tech">tech</div>
+            <div class="tech-list">
+              <div class="laravel tech">laravel</div>
+              <div class="php tech">php</div>
+            </div>
+          </div>
+        </div>
+        <div class="summary">
+          <p>this is travelbug, a social network aimed at connecting travelers to other travelers. it was built
+          using the php framework laravel, along with sass and bootstrap. I envisioned this project as a way
+          for travelers to stay in touch with the friends they make on their travels, while not being as much of a
+          distraction as other social networks tend to be. </p>
+          <img src="img/travelbug.jpg"/>
+        </div>
       </div>
     );
   }
