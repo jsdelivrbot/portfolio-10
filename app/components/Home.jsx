b838bf179be4237ad23f4de9ffdb8547b29d780a
Kevin <katokh89@gmail.com>
2016-06-24 14:39:13 -0700

    remove carousel, begin adding image overlays.

--- app/components/Home.jsx
+++ app/components/Home.jsx
@@ -2,22 +2,27 @@ var React = require('react');
 var Portfolio = require('Portfolio');
 var Travelbug = require('Travelbug');
 var {Link, IndexLink} = require('react-router');
-var Carousel = require('nuka-carousel');
 
+// <div className="image-wrapper overlay-fade-in">
+//   <Link to='travelbug'><h5 className="name">Travelbug</h5><img className="text-center" src={"img/travelbug.jpg"}/></Link>
+//   <div className="image-overlay-content"><h5>travelbug</h5></div>
+// </div>
 var Home = React.createClass({
-
   render: function() {
     return (
       <div className="row home">
         <h5 className="selected-projects">Selected Projects</h5>
         <div className="row">
-          <div className="columns medium-centered medium-10">
-          <Carousel>
-              <Link to='travelbug'><h5>Travelbug</h5><img className="text-center" src={"img/travelbug.jpg"}/></Link>
-              <Link to='pathhop'><h5>pathHop</h5><img className="text-center" src={"img/pathHop.jpg"}/></Link>
-              <Link to='weather'><h5>React Weather</h5><img className="text-center" src={"img/reactWeather.jpg"}/></Link>
-              <Link to='circles'><h5>Circles and Squares of Doom</h5><img className="text-center" src={"img/circles.jpg"}/></Link>
-            </Carousel>
+          <div className="columns medium-centered medium-8">
+              <Link to='travelbug'><h5 className="name">Travelbug</h5>
+                <div className="image-wrapper overlay-fade-in">
+                  <img className="text-center" src={"img/travelbug.jpg"}/>
+                    <div className="image-overlay-content"><h5>travelbug</h5></div>
+                </div>
+              </Link>
+              <Link to='pathhop'><h5 className="name">pathHop</h5><img className="text-center" src={"img/pathHop.jpg"}/></Link>
+              <Link to='weather'><h5 className="name">React Weather</h5><img className="text-center" src={"img/reactWeather.jpg"}/></Link>
+              <Link to='circles'><h5 className="name">Circles and Squares of Doom</h5><img className="text-center" src={"img/circles.jpg"}/></Link>
           </div>
         </div>
       </div>
